4dd3121f104b0f5cb1443dc3f30ed398aa2ce8fb
Prantik Pattanayak <pattanayakprantik@gmail.com>
2021-04-05 00:46:37 +0530

    Third program

---
+++ src/test/java/com/jenkins/config/Test3.java
@@ -0,0 +1,12 @@
+package com.jenkins.config;
+
+import org.testng.annotations.Test;
+
+public class Test3 
+{
+     @Test
+     public void thirdtest()
+     {
+    	 System.out.println("welcome to bangalore");
+     }
+}
