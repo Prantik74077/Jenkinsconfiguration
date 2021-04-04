498d262239c6032c77d3af789f8d12a5e8d59e9e
Prantik Pattanayak <pattanayakprantik@gmail.com>
2021-04-05 01:15:38 +0530

    Bangalore

--- src/test/java/com/jenkins/config/Test1.java
+++ src/test/java/com/jenkins/config/Test1.java
@@ -1,6 +1,6 @@
 package com.jenkins.config;
 
-import org.testng.Reporter;
+import org.testng.Reporter; 
 import org.testng.annotations.Test;
 
 public class Test1 
---
+++ src/test/java/com/jenkins/config/Test4.java
@@ -0,0 +1,13 @@
+package com.jenkins.config;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test4 
+{
+	 @Test
+     public void fourthtest()
+     {
+    	 Reporter.log("Bangalore",true);
+     }
+}
